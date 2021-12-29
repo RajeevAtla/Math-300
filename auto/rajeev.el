832a3025a3ab004c0c271462924a0a89d1c4ee2b
Rajeev Atla <rajeev@rajeevatla.com>
2021-12-29 13:17:29 -0500

    add hw2

--- auto/rajeev.el
+++ auto/rajeev.el
@@ -4,7 +4,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("xcolor" "dvipsnames") ("tcolorbox" "many")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "geometry"
@@ -25,6 +33,7 @@
     '("bm" 1)
     '("subproblemat" 1)
     '("subproblemnt" 1)
+    '("problemt" 1)
     '("recitation" 1)
     '("lecture" 1)
     '("norm" 1)
